1d7aaeafba2916b97f197f1bcb18657865909a58
Azizbek <jonibekov99@gmail.com>
2021-06-14 14:44:40 +0500

    Fix name

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - " + "Aircraft Design",
-    title: "Aircraft Design" || "",
+    titleTemplate: "%s - " + "ArtCraft Production",
+    title: "ArtCraft Production" || "",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
